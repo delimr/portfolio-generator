ad616bff99090eca3e2b53c049d54894597d4c0b
delimr <whoisraf.delima@users.noreply.github.com>
2020-08-08 15:24:54 -1000

    added package.json and .gitignore

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
@@ -26,5 +26,6 @@ const generatePage = (name, github) => {
 fs.writeFile('./index.html', generatePage(name, github), err => {
   if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
+  console.log('Portfolio complete! Check out index.html to see the output!');fs.lstat
 });
+module.exports = generatePage;

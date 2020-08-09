7b31bf26af7fd7ac73717a5024aac15b88d821f4
delimr <whoisraf.delima@users.noreply.github.com>
2020-08-08 15:43:01 -1000

    added inquire

--- app.js
+++ app.js
@@ -1,31 +1,113 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const profileDataArgs = process.argv.slice(2);
+// 
+// const [name, github] = profileDataArgs;
+// 
+// const generatePage = (name, github) => {
+//   return `
+//   <!DOCTYPE html> 
+//   <html lang="en"> 
+//   <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <title>Portfolio Demo</title>
+//   </head>
+// 
+//   <body>
+//     <h1>${name}</h1>
+//     <h2><a href="https://github.com/${github}">Github</a></h2>
+//   </body>
+//   </html>
+//   `;
+// };
+// 
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
+// 
+//   console.log('Portfolio complete! Check out index.html to see the output!');fs.lstat
+// });
+// module.exports = generatePage;
 
-const [name, github] = profileDataArgs;
-
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
-
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
+const inquirer = require('inquirer');
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
 };
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+promptUser().then(answers => console.log(answers));
+const promptProject = portfolioData => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ]);
+};
+promptUser()
+  .then(answers => console.log(answers))
+  .then(promptProject)
+  .then(projectAnswers => console.log(projectAnswers));
 
-  console.log('Portfolio complete! Check out index.html to see the output!');fs.lstat
+  // If there's no 'projects' array property, create one
+if (!portfolioData.projects) {
+  portfolioData.projects = [];
+}
+.then(projectData => {
+  portfolioData.projects.push(projectData);
+  if (projectData.confirmAddProject) {
+    return promptProject(portfolioData);
+  } else {
+    return portfolioData;
+  }
 });
-module.exports = generatePage;
